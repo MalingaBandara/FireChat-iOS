771c9e2115d2d5eb1871f731610769605d3cbc46
Malinga Bandara <65806631+MalingaBandara@users.noreply.github.com>
2022-03-04 21:22:46 +0530

    final stage 1.1

--- Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -1,10 +1,6 @@
 //
 //  WelcomeViewController.swift
 //  Flash Chat iOS13
-//
-//  Created by Angela Yu on 21/10/2019.
-//  Copyright © 2019 Angela Yu. All rights reserved.
-//
 
 import UIKit
 import CLTypingLabel
@@ -15,15 +11,27 @@ class WelcomeViewController: UIViewController {
     
     //To hide navigation bar
     override func viewWillAppear(_ animated: Bool) {
-        <#code#>
+        
+        super.viewWillAppear(animated)
+        
+        navigationController?.isNavigationBarHidden = true // To hide navigation bar
     }
     
+    
+    // to set navigation after Disappear current screen
+    override func viewWillDisappear(_ animated: Bool) {
+        
+        super.viewWillDisappear(animated)
+        
+        navigationController?.isNavigationBarHidden = false // To show navigation bar
+    }
+    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
         titleLabel.text =  K.appName // animation lable
 
-        
 //        titleLabel.text = "" // set empty stringte
 //              let  titleText = "⚡️FlashChat" // text should be display
 //              var charIndex = 0
@@ -41,5 +49,4 @@ class WelcomeViewController: UIViewController {
        
     }
     
-
 }
