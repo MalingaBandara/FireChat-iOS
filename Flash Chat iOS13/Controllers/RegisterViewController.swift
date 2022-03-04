b976578938f94ad42efb2109c34aa39d26495c4b
Malinga Bandara <65806631+MalingaBandara@users.noreply.github.com>
2022-03-04 14:24:41 +0530

    final stage 1

--- Flash Chat iOS13/Controllers/RegisterViewController.swift
+++ Flash Chat iOS13/Controllers/RegisterViewController.swift
@@ -14,6 +14,7 @@ class RegisterViewController: UIViewController {
     @IBOutlet weak var emailTextfield: UITextField!
     @IBOutlet weak var passwordTextfield: UITextField!
     
+    
     @IBAction func registerPressed(_ sender: UIButton) {
         
         
